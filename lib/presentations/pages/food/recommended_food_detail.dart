bca8f55fcea147c1ac89e48fc9caed46b27aa708
nouletame <nouletamemarcel@gmail.com>
2022-08-12 12:56:54 +0000

    ui end #2

--- lib/presentations/pages/food/recommended_food_detail.dart
+++ lib/presentations/pages/food/recommended_food_detail.dart
@@ -65,16 +65,81 @@ class RecommendedFoodDetail extends StatelessWidget {
         ],
       ),
       bottomNavigationBar: Column(mainAxisSize: MainAxisSize.min, children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            AppIcon(
-                backgoundColor: AppColors.mainColor,
-                iconColor: Colors.white,
-                icon: Icons.remove),
-            AppIcon(icon: Icons.add),
-          ],
-        )
+        Container(
+          padding: EdgeInsets.only(
+              left: Dimensions.widtht20 * 2.5,
+              right: Dimensions.widtht20 * 2.5,
+              top: Dimensions.height10,
+              bottom: Dimensions.height10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              AppIcon(
+                  backgoundColor: AppColors.mainColor,
+                  iconSize: Dimensions.iconseSize24,
+                  iconColor: Colors.white,
+                  icon: Icons.remove),
+              BigText(
+                text: '150 CFA',
+                color: AppColors.mainBlackColor,
+                size: Dimensions.font26,
+              ),
+              AppIcon(
+                  backgoundColor: AppColors.mainColor,
+                  iconSize: Dimensions.iconseSize24,
+                  iconColor: Colors.white,
+                  icon: Icons.add),
+            ],
+          ),
+        ),
+        Container(
+          height: Dimensions.bottomHeighBar,
+          padding: EdgeInsets.only(
+              top: Dimensions.height30,
+              bottom: Dimensions.height30,
+              left: Dimensions.widtht20,
+              right: Dimensions.widtht20),
+          decoration: BoxDecoration(
+            color: AppColors.buttonBackgroundColor,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(Dimensions.radius20 * 2),
+              topRight: Radius.circular(Dimensions.radius20 * 2),
+            ),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(
+                  vertical: Dimensions.height20,
+                  horizontal: Dimensions.widtht20,
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(Dimensions.radius20),
+                  color: Colors.white,
+                ),
+                child: Icon(
+                  Icons.favorite,
+                  color: AppColors.mainColor,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(
+                  vertical: Dimensions.height20,
+                  horizontal: Dimensions.widtht20,
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(Dimensions.radius20),
+                  color: AppColors.mainColor,
+                ),
+                child: BigText(
+                  text: '150F | Add To cart',
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
       ]),
     );
   }
